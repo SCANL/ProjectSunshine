fc20df5667763a785cdf7c13be549fe26d75bcd1
shehan <shehan.peruma@gmail.com>
2021-02-16 15:37:10 -0500

    updated the mechanism to capture data types of attributes, parameters and variables

--- model/entity.py
+++ model/entity.py
@@ -20,11 +20,21 @@ class Entity:
             model_class = Class(class_name.text, class_item)
 
             attribute_list = class_item.xpath('*/src:decl_stmt/src:decl', namespaces={'src': 'http://www.srcML.org/srcML/src'})
-            for attribute_item in attribute_list:
+            for i, attribute_item in enumerate(attribute_list):
                 attribute_name = attribute_item.xpath('./src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0]
-                attribute_type = attribute_item.xpath('./src:type/src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0]
-                model_attribute = Attribute(attribute_type.text, attribute_name.text, model_class.name+"."+attribute_name.text,  attribute_item)
-                model_class.attribute.append((model_attribute))
+                attribute_type = attribute_item.xpath('./src:type/src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})
+
+                if len(attribute_type) != 0:
+                    attribute_type = attribute_type[0]
+                    if attribute_type.text == None:
+                        attribute_type = attribute_item.xpath('./src:type/src:name/src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0]
+                    model_attribute = Attribute(attribute_type.text, attribute_name.text,model_class.name + "." + attribute_name.text, attribute_item)
+                else:# capture the attribute's type when the attributes are declared in groups (e.g., String app, testFilePath;)
+                    if attribute_item.xpath('./src:type/@ref', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0] == 'prev':
+                        attribute_type = model_class.attribute[-1].type
+                        model_attribute = Attribute(attribute_type, attribute_name.text, model_class.name + "." + attribute_name.text, attribute_item)
+
+                model_class.attribute.append(model_attribute)
 
             method_list = class_item.xpath('*/src:function', namespaces={'src': 'http://www.srcML.org/srcML/src'})
             for method_item in method_list:
@@ -37,8 +47,14 @@ class Entity:
                 parameter_list = method_item.xpath('*/src:parameter/src:decl', namespaces={'src': 'http://www.srcML.org/srcML/src'})
                 for parameter_item in parameter_list:
                     parameter_name = parameter_item.xpath('./src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0]
-                    parameter_type = parameter_item.xpath('./src:type/src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0]
-                    model_parameter = Parameter(parameter_type.text, parameter_name.text, parameter_item)
+                    parameter_type = parameter_item.xpath('./src:type/src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})
+
+                    if len(parameter_type) != 0:
+                        parameter_type = parameter_type[0]
+                        if parameter_type.text == None:
+                            parameter_type = parameter_item.xpath('./src:type/src:name/src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0]
+                        model_parameter = Parameter(parameter_type.text, parameter_name.text, parameter_item)
+
                     model_method.parameters.append(model_parameter)
 
                 for parameter_item in model_method.parameters:
@@ -47,8 +63,18 @@ class Entity:
                 variable_list = method_item.xpath('*//src:decl_stmt/src:decl', namespaces={'src': 'http://www.srcML.org/srcML/src'})
                 for variable_item in variable_list:
                     variable_name = variable_item.xpath('./src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0]
-                    variable_type = variable_item.xpath('./src:type/src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0]
-                    model_variable = Variable(variable_type.text, variable_name.text, variable_item)
+                    variable_type = variable_item.xpath('./src:type/src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})
+
+                    if len(variable_type) != 0:
+                        variable_type = variable_type[0]
+                        if variable_type.text == None:
+                            variable_type = variable_item.xpath('./src:type/src:name/src:name', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0]
+                        model_variable = Variable(variable_type.text, variable_name.text, variable_item)
+                    else:  # capture the variable's type when the variables are declared in groups (e.g., String app, testFilePath;)
+                        if variable_item.xpath('./src:type/@ref', namespaces={'src': 'http://www.srcML.org/srcML/src'})[0] == 'prev':
+                            variable_type = model_method.variables[-1].type
+                            model_variable = Variable(variable_type, variable_name.text, variable_item)
+
                     model_method.variables.append(model_variable)
 
                 for variable_item in model_method.variables:
