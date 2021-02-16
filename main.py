2d2974c78107f677c729f0e7f37e7adadb75f68e
shehan <shehan.peruma@gmail.com>
2021-02-16 12:44:21 -0500

    handle command line arguments

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
+import os
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+
 from analyzer import Analyzer
+from common import util
 from model.file_type import FileType
 
 
-def run_analysis():
-    file_path = 'C:/Users/sheha/Downloads/GreetingTest.java'
-    file_type = FileType.Test
-    a = Analyzer(file_path, file_type)
-    a.analyze()
+def run_analysis(files):
+    # file_path = 'C:/Users/sheha/Downloads/GreetingTest.java'
+    for file in files:
+        print("Analyzing: %s ..." % (str(file)), end='', flush=True)
+        file_type = FileType.Test
+        a = Analyzer(str(file), file_type)
+        a.analyze()
+        print('done!')
+
+
+def process_arguments():
+    parser = ArgumentParser(util.get_config_setting('general', 'name'))
+    parser.add_argument("-f", "--file", dest="arg_file", required=True,
+                        help="Source file or directory to be analyzed", metavar="FILE")
+    args = parser.parse_args()
+
+    if not os.path.exists(args.arg_file):
+        sys.exit("Invalid file/directory supplied")
+
+    if os.path.isdir(args.arg_file):
+        path = Path(args.arg_file)
+        files = list(path.rglob('*.java'))
+        if len(files) == 0:
+            sys.exit("Java files not present in specified directory")
+        else:
+            run_analysis(files)
+
+    if os.path.isfile(args.arg_file):
+        if not args.arg_file.lower().endswith('.java'):
+            sys.exit("Specified file must be a Java source file")
+        else:
+            run_analysis([args.arg_file])
 
 
 if __name__ == '__main__':
-    run_analysis()
+    process_arguments()
