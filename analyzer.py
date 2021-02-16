9d46ed94911b3ec39d9fb543a7969781776283a3
shehan <shehan.peruma@gmail.com>
2021-02-16 12:44:01 -0500

    updated

--- analyzer.py
+++ analyzer.py
@@ -3,6 +3,7 @@ from rule.test.annotation_setup import AnnotationSetup
 from rule.test.annotation_teardown import AnnotationTeardown
 from rule.test.annotation_test import AnnotationTest
 from service.factory import EntityFactory
+from service.result_writer import ResultWriter
 
 
 class Analyzer:
@@ -11,11 +12,17 @@ class Analyzer:
         self.file_path = file_path
         self.file_type = file_type
         self.junit = None
-        self.rules=[AnnotationTest(), AnnotationSetup(), AnnotationTeardown()]
+        self.rules = [AnnotationTest(), AnnotationSetup(), AnnotationTeardown()]
+        self.issues = []
 
     def analyze(self):
         if self.file_type == FileType.Test:
             entity = EntityFactory().construct_model(self.file_path)
             for rule in self.rules:
-                rule.analyze(entity)
-                print('---')
+                self.issues.append(rule.analyze(entity))
+
+        results = ResultWriter()
+        for issue in self.issues:
+            if type(issue) is not type(None):
+                for sub_issue in issue:
+                    results.save_issue(sub_issue)
