968a6d130e1ab9b25a1d7c5d13b298961748fee9
shehan <shehan.peruma@gmail.com>
2021-02-27 21:58:26 -0500

    updated

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,4 @@
-from model.file_type import FileType
+from rule.linguistic_antipattern.get_no_return import GetNoReturn
 from rule.test.annotation_setup import AnnotationSetup
 from rule.test.annotation_teardown import AnnotationTeardown
 from rule.test.annotation_test import AnnotationTest
@@ -13,6 +13,7 @@ class Analyzer:
         self.file_type = file_type
         self.junit = None
         self.rules = [
+            GetNoReturn(),
             AnnotationTest(),
             AnnotationSetup(),
             AnnotationTeardown(),
@@ -21,12 +22,14 @@ class Analyzer:
         self.issues = []
 
     def analyze(self):
-        if self.file_type == FileType.Test:
-            entity = EntityFactory().construct_model(self.file_path)
-            for rule in self.rules:
-                issue_list = rule.analyze(entity)
-                if issue_list is not None:
-                    self.issues.append(issue_list)
+        entity = EntityFactory().construct_model(self.file_path)
+        entity.set_file_type(self.file_type)
+        entity.junit = self.junit
+
+        for rule in self.rules:
+            issue_list = rule.analyze(entity)
+            if issue_list is not None:
+                self.issues.append(issue_list)
 
         concat_issues = [j for i in self.issues for j in i]
         return concat_issues
