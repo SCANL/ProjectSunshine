376d28a7773772f5a35bab38fec72bc2cbdb41d5
shehan <shehan.peruma@gmail.com>
2021-03-13 10:43:18 -0500

    lists of custom terms

---
+++ nlp/custom_terms.py
@@ -0,0 +1,19 @@
+boolean_terms = ['is',
+                 'has',
+                 'have',
+                 'can']
+
+get_terms = ['find',
+             'fetch',
+             'query',
+             'generate',
+             'produce',
+             'obtain',
+             'acquire',
+             'develop',
+             'return',
+             'recall',
+             'render',
+             'yield',
+             'deliver',
+             'give']
