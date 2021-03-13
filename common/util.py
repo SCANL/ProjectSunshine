1ec3310349e42507d70730bb5b794e0e2aeeb569
shehan <shehan.peruma@gmail.com>
2021-03-13 12:25:32 -0500

    list of java primitive and collection data types

--- common/util.py
+++ common/util.py
@@ -20,3 +20,51 @@ def get_config_setting(section, name):
 def get_file_name(file_path):
     head, tail = os.path.split(file_path)
     return tail
+
+
+# https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+java_primitive_data_types = ['byte', 'short', 'int', 'long', 'float', 'double', 'boolean', 'char']
+
+# https://docs.oracle.com/javase/8/docs/technotes/guides/collections/overview.html
+java_collection_data_types = [
+    'ArrayBlockingQueue',
+    'ArrayDeque',
+    'ArrayList',
+    'BlockingDeque',
+    'BlockingQueue',
+    'ConcurrentHashMap',
+    'ConcurrentMap',
+    'ConcurrentNavigableMap',
+    'ConcurrentSkipListMap',
+    'ConcurrentSkipListSet',
+    'CopyOnWriteArrayList',
+    'CopyOnWriteArraySet',
+    'DelayQueue',
+    'Deque',
+    'HashMap',
+    'HashSet',
+    'Hashtable',
+    'Iterator ',
+    'LinkedBlockingDeque',
+    'LinkedBlockingQueue',
+    'LinkedHashMap',
+    'LinkedHashSet',
+    'LinkedList',
+    'LinkedTransferQueue',
+    'List',
+    'ListIterator',
+    'Map',
+    'NavigableMap',
+    'NavigableSet',
+    'PriorityBlockingQueue',
+    'PriorityQueue',
+    'Queue',
+    'Set',
+    'SortedMap',
+    'SortedSet',
+    'SynchronousQueue',
+    'TransferQueue',
+    'TreeMap',
+    'TreeSet',
+    'Vector'
+]
