da23d3b92d2d448309481b27572d6482d3ead9cc
shehan <shehan.peruma@gmail.com>
2021-03-14 10:58:21 -0400

    updated condition

--- rule/linguistic_antipattern/get_more_than_accessor.py
+++ rule/linguistic_antipattern/get_more_than_accessor.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from common.util_parsing import get_all_conditional_statements
+from common.util_parsing import get_all_conditional_statements, get_class_attribute_names
 from model.identifier_type import get_type
 from model.issue import Issue
 
@@ -13,26 +13,29 @@ class GetMoreThanAccessor:
         self.__issues = []
         self.__issue_category = '\'Get\' more than accessor'
         self.__issue_description = 'A getter that performs actions other than returning the corresponding attribute.'
+        self.__class_attributes = None
 
     def __process_identifier(self, identifier):
-        # Issue: The name starts with 'get' AND the method body contains conditional statements (if, loops, switch)
+        # Issue: The method name starts with 'get' AND the mehod is a getter for an attribute AND the method body contains conditional statements (if, loops, switch)
         if identifier.name_terms[0].lower() == 'get':
-            conditional_statements, conditional_statements_total = get_all_conditional_statements(identifier.source)
+            if identifier.name.lower().endswith(tuple([item.lower() for item in self.__class_attributes])):
+                conditional_statements, conditional_statements_total = get_all_conditional_statements(identifier.source)
 
-            if conditional_statements_total != 0:
-                issue = Issue()
-                issue.file_path = self.__entity.path
-                issue.identifier = identifier.get_fully_qualified_name()
-                issue.identifier_type = get_type(type(identifier).__name__)
-                issue.category = self.__issue_category
-                issue.details = self.__issue_description
-                issue.analysis_datetime = datetime.now()
-                self.__issues.append(issue)
+                if conditional_statements_total != 0:
+                    issue = Issue()
+                    issue.file_path = self.__entity.path
+                    issue.identifier = identifier.get_fully_qualified_name()
+                    issue.identifier_type = get_type(type(identifier).__name__)
+                    issue.category = self.__issue_category
+                    issue.details = self.__issue_description
+                    issue.analysis_datetime = datetime.now()
+                    self.__issues.append(issue)
 
     def analyze(self, entity):
         # Analyze all methods in a class
         self.__entity = entity
         for class_item in self.__entity.classes:
+            self.__class_attributes = get_class_attribute_names(class_item)
             for method_item in class_item.methods:
                 self.__process_identifier(method_item)
 
