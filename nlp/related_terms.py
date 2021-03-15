4158ef100317edbe97957e18ce2349d2eae63189
shehan <shehan.peruma@gmail.com>
2021-03-15 16:06:49 -0400

    method to check if two terms are antonyms

--- nlp/related_terms.py
+++ nlp/related_terms.py
@@ -1,6 +1,26 @@
-from itertools import chain
+from itertools import product
 
 from nltk.corpus import wordnet
+
+
+def are_antonyms(term1, term2):
+    match = False
+
+    syns1 = wordnet.synsets(term1.lower())
+    syns2 = wordnet.synsets(term2.lower())
+
+    for sys1, sys2 in product(syns1, syns2):
+        for lemma1, lemma2 in product(sys1.lemmas(), sys2.lemmas()):
+            for antonym1, lemma2 in product(lemma1.antonyms(), sys2.lemmas()):
+                if antonym1.name() == lemma2.name():
+                    match = True
+                    break
+            for lemma1, antonym2 in product(sys1.lemmas(), lemma2.antonyms()):
+                if antonym2.name() == lemma1.name():
+                    match = True
+                    break
+
+    return match
 
 
 def get_synonyms(term, pos):
@@ -11,8 +31,8 @@ def get_synonyms(term, pos):
             synonym_terms.extend(synonym.lemma_names())
 
     return set(synonym_terms)
-    #return set(chain.from_iterable([word.lemma_names() for word in synonyms]))
+    # return set(chain.from_iterable([word.lemma_names() for word in synonyms]))
 
 
-#z = get_synonyms('return','v')
+#z = are_antonyms('sleep', 'run')#get_synonyms('to', 'v')
 #print(z)
