6dfca9e5a197c414357ceac8403e13b50cd6f517
shehan <shehan.peruma@gmail.com>
2021-03-18 10:23:47 -0400

    encapsulate values within quotes

--- service/result_writer.py
+++ service/result_writer.py
@@ -9,12 +9,12 @@ class ResultWriter:
             self.results_file = open(results_file, 'a', encoding="utf-8")
         else:
             self.results_file = open(results_file, 'w', encoding="utf-8")
-            self.results_file.write('FilePath,Identifier,IdentifierType,IssueCategory,IssueDetail,AnalysisDateTime\n')
+            self.results_file.write('"FilePath","Identifier","IdentifierType","IssueCategory","IssueDetail","AnalysisDateTime"\n')
             self.results_file.flush()
 
     def save_issues(self, issues):
         for issue in issues:
-            self.results_file.write('%s,%s,%s,%s,%s,%s\n' % (
+            self.results_file.write('"%s","%s","%s","%s","%s","%s"\n' % (
                 issue.file_path, issue.identifier, issue.identifier_type.name, issue.category, issue.details,
                 issue.analysis_datetime.strftime("%Y-%m-%d %H:%M:%S")))
             self.results_file.flush()
