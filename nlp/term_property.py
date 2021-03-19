ae3c9796c862b2a0cf15ded85616f0533c942cf7
shehan <shehan.peruma@gmail.com>
2021-03-19 12:02:28 -0400

    custom plural terms

--- nlp/term_property.py
+++ nlp/term_property.py
@@ -1,7 +1,10 @@
 from nlp.pos_tag import generate_tag
 
+custom_plural_terms = ['apps']
 
 def is_singular(term):
+    if term in custom_plural_terms:
+        return False
     tag = generate_tag(term)
     if tag == 'NN' or tag == 'NNP':
         return True
@@ -10,6 +13,8 @@ def is_singular(term):
 
 
 def is_plural(term):
+    if term in custom_plural_terms:
+        return True
     tag = generate_tag(term)
     if tag == 'NNS' or tag == 'NNPS':
         return True
