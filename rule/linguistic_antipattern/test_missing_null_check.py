17db5e3f59645e0c2fc224d21facc315a7da3c96
shehan <shehan.peruma@gmail.com>
2021-04-28 18:27:35 -0400

    new anti-pattern: Test method does not contain a null check

---
+++ rule/linguistic_antipattern/test_missing_null_check.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+from common.enum import FileType, IdentifierType
+from common.util import get_null_check_test_method
+from common.util_parsing import get_all_function_calls
+from model.issue import Issue
+from nlp import pos_tag
+from nlp.pos_tag import POSType
+
+
+class TestMissingNullCheck:
+
+    def __init__(self):
+        self.__entity = None
+        self.__id = 'P.5'
+        self.__junit = 4  # None
+        self.__issues = []
+        self.__issue_category = 'Test method missing null check'
+        self.__issue_description = 'Body of the test method is missing a null check even though the name contains the term \'null\' or \'not\' '
+
+    def __get_junit_version(self):
+        pass
+
+    def __process_identifier(self, identifier):
+        # AntiPattern: Method name contains the term 'null' or 'not', but does not perform a null check
+        if 'null' in map(str.lower, identifier.name_terms) or 'not' in map(str.lower, identifier.name_terms):
+            method_calls = get_all_function_calls(identifier.source)
+            api_null_method = get_null_check_test_method(self.__entity.language)
+            if not any(x in method_calls for x in api_null_method):
+                issue = Issue()
+                issue.file_path = self.__entity.path
+                issue.identifier = identifier.get_fully_qualified_name()
+                issue.identifier_type = IdentifierType.get_type(type(identifier).__name__)
+                issue.category = self.__issue_category
+                issue.details = self.__issue_description
+                issue.id = self.__id
+                issue.analysis_datetime = datetime.now()
+                self.__issues.append(issue)
+
+    def analyze(self, entity):
+        if entity.file_type == FileType.Test:
+            self.__entity = entity
+            for class_item in self.__entity.classes:
+                for method_item in class_item.methods:
+                    self.__process_identifier(method_item)
+
+        return self.__issues
