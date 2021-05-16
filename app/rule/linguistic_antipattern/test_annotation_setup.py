e31e4c1e80f44fe7049a18309875abb7b1f03a71
shehan <shehan.peruma@gmail.com>
2021-05-16 19:57:41 -0400

    updated

--- app/rule/linguistic_antipattern/test_annotation_setup.py
+++ app/rule/linguistic_antipattern/test_annotation_setup.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from app.common.enum import FileType, IdentifierType
+from app.common.enum import FileType, IdentifierType, LanguageType
 from app.common.error_handler import ErrorSeverity, handle_error
 from app.model.issue import Issue
 
@@ -23,28 +23,28 @@ class TestAnnotationSetup:
         pass
 
     def __process_identifier(self, identifier):
-        #if self.__junit >= 4:
-        try:
-            if len(identifier.name_terms) == 1 and \
-                    identifier.name_terms[0].lower() == 'setup' and \
-                    'Before' not in identifier.annotations:
-                issue = Issue()
-                issue.file_path = self.__entity.path
-                issue.identifier = identifier.get_fully_qualified_name()
-                issue.identifier_type = IdentifierType.get_type(type(identifier).__name__)
-                issue.category = self.__issue_category
-                issue.details = self.__issue_description
-                issue.id = self.__id
-                issue.analysis_datetime = datetime.now()
-                issue.file_type = self.__entity.file_type
-                issue.line_number = identifier.line_number
-                issue.column_number = identifier.column_number
-                self.__issues.append(issue)
-        except Exception as e:
-            error_message = "Error encountered processing %s in file %s [%s:%s]" % (
-                IdentifierType.get_type(type(identifier).__name__), self.__entity.path, identifier.line_number,
-                identifier.column_number)
-            handle_error('X.1', error_message, ErrorSeverity.Error, False, e)
+        if self.__entity.language == LanguageType.Java and self.__junit >= 4:
+            try:
+                if len(identifier.name_terms) == 1 and \
+                        identifier.name_terms[0].lower() == 'setup' and \
+                        'Before' not in identifier.annotations:
+                    issue = Issue()
+                    issue.file_path = self.__entity.path
+                    issue.identifier = identifier.get_fully_qualified_name()
+                    issue.identifier_type = IdentifierType.get_type(type(identifier).__name__)
+                    issue.category = self.__issue_category
+                    issue.details = self.__issue_description
+                    issue.id = self.__id
+                    issue.analysis_datetime = datetime.now()
+                    issue.file_type = self.__entity.file_type
+                    issue.line_number = identifier.line_number
+                    issue.column_number = identifier.column_number
+                    self.__issues.append(issue)
+            except Exception as e:
+                error_message = "Error encountered processing %s in file %s [%s:%s]" % (
+                    IdentifierType.get_type(type(identifier).__name__), self.__entity.path, identifier.line_number,
+                    identifier.column_number)
+                handle_error('X.1', error_message, ErrorSeverity.Error, False, e)
 
     def analyze(self, project, entity):
         if entity.file_type == FileType.Test:
