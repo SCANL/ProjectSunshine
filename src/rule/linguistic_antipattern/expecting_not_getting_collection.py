9c3092092e74519edd05241a5e372f8b56f528ac
shehan <shehan.peruma@gmail.com>
2021-05-29 12:52:47 -0400

    corrected the anti-pattern condition.

--- src/rule/linguistic_antipattern/expecting_not_getting_collection.py
+++ src/rule/linguistic_antipattern/expecting_not_getting_collection.py
@@ -5,6 +5,7 @@ from src.common.error_handler import handle_error, ErrorSeverity
 from src.common.types_list import get_collection_types
 from src.common.util_parsing import is_test_method
 from src.model.issue import Issue
+from src.nlp import term_list
 from src.nlp.term_property import is_plural
 
 
@@ -23,23 +24,25 @@ class ExpectingNotGettingCollection:
         self.__issue_description = 'The name of a method suggests that a collection should be returned but a single object or nothing is returned.'
 
     def __process_identifier(self, identifier):
-        # AntiPattern: if the last term is plural and the return type is not a collection
+        # AntiPattern: if the fist term is a get related term AND [(any term is plural or contains collection term)] and the return type is not a collection
         try:
             if not is_test_method(self.__project, self.__entity, identifier):
-                if is_plural(self.__project, identifier.name_terms[-1]):
-                    if identifier.return_type not in get_collection_types(self.__project, self.__entity.language) and identifier.is_array != True:
-                        issue = Issue()
-                        issue.file_path = self.__entity.path
-                        issue.identifier = identifier.get_fully_qualified_name()
-                        issue.identifier_type = IdentifierType.get_type(type(identifier).__name__)
-                        issue.category = self.__issue_category
-                        issue.details = self.__issue_description
-                        issue.id = self.__id
-                        issue.analysis_datetime = datetime.now()
-                        issue.file_type = self.__entity.file_type
-                        issue.line_number = identifier.line_number
-                        issue.column_number = identifier.column_number
-                        self.__issues.append(issue)
+                if identifier.name_terms[0].lower() in term_list.get_get_terms(self.__project):
+                    if any(is_plural(self.__project, c) for c in identifier.name_terms[1:]) or any(item in identifier.name_terms[1:] for item in get_collection_types(self.__project, self.__entity.language)):
+                        if identifier.return_type not in get_collection_types(self.__project, self.__entity.language) and identifier.is_array != True:
+                            issue = Issue()
+                            issue.file_path = self.__entity.path
+                            issue.identifier = identifier.get_fully_qualified_name()
+                            issue.identifier_type = IdentifierType.get_type(type(identifier).__name__)
+                            issue.category = self.__issue_category
+                            issue.details = self.__issue_description
+                            issue.additional_details = 'Return type: %s' % identifier.return_type
+                            issue.id = self.__id
+                            issue.analysis_datetime = datetime.now()
+                            issue.file_type = self.__entity.file_type
+                            issue.line_number = identifier.line_number
+                            issue.column_number = identifier.column_number
+                            self.__issues.append(issue)
         except Exception as e:
             error_message = "Error encountered processing %s in file %s [%s:%s]" % (
                 IdentifierType.get_type(type(identifier).__name__), self.__entity.path, identifier.line_number,
