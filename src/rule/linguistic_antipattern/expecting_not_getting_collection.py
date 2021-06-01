d82e6eebe7b65ce056290068d04f43205d134c30
shehan <shehan.peruma@gmail.com>
2021-06-01 11:08:01 -0400

    if the data type is numeric, show a message that this might be a false positive

--- src/rule/linguistic_antipattern/expecting_not_getting_collection.py
+++ src/rule/linguistic_antipattern/expecting_not_getting_collection.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from src.common.enum import IdentifierType
 from src.common.error_handler import handle_error, ErrorSeverity
-from src.common.types_list import get_collection_types
+from src.common.types_list import get_collection_types, get_numeric_types
 from src.common.util_parsing import is_test_method
 from src.model.issue import Issue
 from src.nlp import term_list
@@ -30,13 +30,16 @@ class ExpectingNotGettingCollection:
                 if identifier.name_terms[0].lower() in term_list.get_get_terms(self.__project):
                     if any(is_plural(self.__project, c) for c in identifier.name_terms[1:]) or any(item in map(str.lower, identifier.name_terms[1:]) for item in map(str.lower, get_collection_types(self.__project, self.__entity.language))):
                         if identifier.return_type not in get_collection_types(self.__project, self.__entity.language) and identifier.is_array != True:
+                            message = 'Return type: %s' % identifier.return_type
+                            if identifier.return_type in get_numeric_types(self.__project):
+                                message = message + 'Return type is numeric, this might be a false-positive'
                             issue = Issue()
                             issue.file_path = self.__entity.path
                             issue.identifier = identifier.get_fully_qualified_name()
                             issue.identifier_type = IdentifierType.get_type(type(identifier).__name__)
                             issue.category = self.__issue_category
                             issue.details = self.__issue_description
-                            issue.additional_details = 'Return type: %s' % identifier.return_type
+                            issue.additional_details = message
                             issue.id = self.__id
                             issue.analysis_datetime = datetime.now()
                             issue.file_type = self.__entity.file_type
--- src/rule/linguistic_antipattern/says_many_contains_one.py
+++ src/rule/linguistic_antipattern/says_many_contains_one.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from src.common.enum import IdentifierType
 from src.common.error_handler import handle_error, ErrorSeverity
-from src.common.types_list import get_collection_types
+from src.common.types_list import get_collection_types, get_numeric_types
 from src.common.util_parsing import get_all_class_fields
 from src.model.issue import Issue
 from src.nlp.term_property import is_plural
@@ -23,13 +23,16 @@ class SaysManyContainsOne:
         try:
             if is_plural(self.__project, identifier.name_terms[-1]):
                 if identifier.type not in get_collection_types(self.__project, self.__entity.language) and identifier.is_array != True:
+                    message = 'Last term: \'%s\' Data type: \'%s\'' % (identifier.name_terms[-1], identifier.type)
+                    if identifier.type in get_numeric_types(self.__project):
+                        message = message + 'Data type is numeric, this might be a false-positive'
                     issue = Issue()
                     issue.file_path = self.__entity.path
                     issue.identifier = identifier.get_fully_qualified_name()
                     issue.identifier_type = IdentifierType.get_type(type(identifier).__name__)
                     issue.category = self.__issue_category
                     issue.details = self.__issue_description
-                    issue.additional_details = 'Last term: \'%s\' Data type: \'%s\'' % (identifier.name_terms[-1], identifier.type)
+                    issue.additional_details = message
                     issue.id = self.__id
                     issue.analysis_datetime = datetime.now()
                     issue.file_type = self.__entity.file_type
