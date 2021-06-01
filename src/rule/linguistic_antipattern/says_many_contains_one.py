85b8ee8638bd615ba0ea18643d7d09ee9197c8a0
shehan <shehan.peruma@gmail.com>
2021-06-01 11:58:09 -0400

    updated message to indicate if the data type is boolean and starting term is boolean-based

--- src/rule/linguistic_antipattern/says_many_contains_one.py
+++ src/rule/linguistic_antipattern/says_many_contains_one.py
@@ -2,9 +2,10 @@ from datetime import datetime
 
 from src.common.enum import IdentifierType
 from src.common.error_handler import handle_error, ErrorSeverity
-from src.common.types_list import get_collection_types, get_numeric_types
+from src.common.types_list import get_collection_types, get_numeric_types, get_bool_types
 from src.common.util_parsing import get_all_class_fields
 from src.model.issue import Issue
+from src.nlp import term_list
 from src.nlp.term_property import is_plural
 
 
@@ -26,6 +27,8 @@ class SaysManyContainsOne:
                     message = 'Last term: \'%s\' Data type: \'%s%s\'' % (identifier.name_terms[-1], identifier.type, '(array)' if identifier.is_array else '')
                     if identifier.type in get_numeric_types(self.__entity.language):
                         message = message + '; Data type is numeric, this might be a false-positive'
+                    if identifier.type in get_bool_types(self.__entity.language) and (identifier.name_terms[0].lower() in term_list.get_boolean_terms(self.__project) or identifier.name_terms[0].lower() in term_list.get_validate_terms(self.__project)):
+                        message = message + '; Data type is boolean and starting term is boolean-based, this might be a false-positive'
                     issue = Issue()
                     issue.file_path = self.__entity.path
                     issue.identifier = identifier.get_fully_qualified_name()
