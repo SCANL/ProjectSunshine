1c270f2050e61b5417611cac26c15b3c06c27910
antgioia <antgioia10@gmail.com>
2023-08-23 23:43:08 +0200

    test: add unit tests for src.service.factory
    add unit tests as defined in TP document for src.service.factory

---
+++ test/service/test_factory.py
@@ -0,0 +1,63 @@
+import pytest
+from unittest.mock import patch
+from src.service.factory import EntityFactory
+
+
+class TestFactory:
+    """
+        Test case specification for these test cases can be found here:
+        https://t.ly/0CCGA
+    """
+
+    @pytest.fixture
+    def temp_input_file(self, tmpdir):
+        content = '''
+        public class Main {
+            public static void main(String[] args) {
+                System.out.println("Hello World!");
+            }
+        }
+        '''
+        file_path = tmpdir.join("temp_input.java")
+        file_path.write(content)
+        yield file_path
+        file_path.remove()
+
+    @pytest.fixture
+    def mock_parse_file(self, mocker):
+        """
+            Mock for src.service.parser.Parser.parse_file (used in src.service.factory.EntityFactory.construct_model)
+        """
+        return mocker.patch("src.service.parser.Parser.parse_file")
+
+    @pytest.fixture
+    def mock_construct_hierarchy(self, mocker):
+        """
+            src.model.entity.Entity.construct_hierarchy (used in src.service.factory.EntityFactory.construct_model)
+        """
+        return mocker.patch("src.model.entity.Entity.construct_hierarchy")
+
+    def test_construct_model_with_invalid_input(self, mock_parse_file):
+        """
+            ID: TC-SRV-2.1
+        """
+        mock_parse_file.return_value = False
+        factory = EntityFactory()
+        invalid_file_path = "path/to/invalid/file.java"
+        assert factory.construct_model(invalid_file_path, "", "") == None
+
+    def test_construct_model_with_valid_input(self, temp_input_file, mock_parse_file, mock_construct_hierarchy):
+        """
+            ID: TC-SRV-2.2
+        """
+        mock_parse_file.return_value = True
+        mock_construct_hierarchy.return_value = []
+
+        factory = EntityFactory()
+
+        valid_file_path = str(temp_input_file)
+        factory.construct_model(valid_file_path, "", "")
+
+        assert factory.entity is not None
+        assert factory.entity.path == valid_file_path
+        assert factory.entity.name == "temp_input.java"
