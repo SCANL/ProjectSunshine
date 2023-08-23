480eea3d1023ec6ac4c27b5f0c36e2995a18d4cd
Antonio Scognamiglio <antonio.scognamiglio12@outlook.it>
2023-08-23 23:00:15 +0200

    test: fixed some flakinesses in util test
    - added PATH constant for temp files

--- test/common/test_util.py
+++ test/common/test_util.py
@@ -1,72 +1,77 @@
-import pytest, os, shutil
+import pytest
+import os
+import shutil
 from src.common.util import get_file_name, read_input, remove_list_nestings, get_config_setting
+
 
 class TestUtil:
 
+    PATH = "./test/temp/"
+
     def __create_test_dir(self):
-        if not os.path.exists("tests/test_files"):
-            os.mkdir("tests/test_files")
-        
-        if not os.path.exists("tests/test_files/code"):
-            os.mkdir("tests/test_files/code")
+        if not os.path.exists(self.PATH):
+            os.mkdir(self.PATH)
 
-        if not os.path.exists("tests/test_files/code/project"):
-            os.mkdir("tests/test_files/code/project")
+        if not os.path.exists(f"{self.PATH}/code"):
+            os.mkdir(f"{self.PATH}/code")
 
-    def __delate_files(self):
-        shutil.rmtree("tests/test_files")
-        
+        if not os.path.exists(f"{self.PATH}/code/project"):
+            os.mkdir(f"{self.PATH}/code/project")
+
+    def __delete_files(self):
+        shutil.rmtree(self.PATH)
+
     @pytest.fixture
     def create_correct_files(self):
         self.__create_test_dir()
 
-        with open("tests/test_files/code/project/Main.java", "a") as code:
+        with open(f"{self.PATH}/code/project/Main.java", "a") as code:
             code.write("public class Main {\n")
             code.write("    public static void main(String[] args){\n")
             code.write("        system.out.println(\"hello world\");\n")
             code.write("    }\n}")
-            
-        with open("tests/test_files/input_test.csv", "a") as input:
+
+        with open(f"{self.PATH}/input_test.csv", "a") as input:
             input.write("file,type,junit\n")
-            input.write("tests/test_files/code/project")
+            input.write(f"{self.PATH}/code/project")
 
         yield
 
-        self.__delate_files()
-
+        self.__delete_files()
 
     @pytest.fixture
     def create_empty_csv(self):
         self.__create_test_dir()
-            
-        with open("tests/test_files/input_test.csv", "a") as csv:
+
+        with open(f"{self.PATH}/input_test.csv", "a") as csv:
             csv.write("file,type,junit")
 
         yield
 
-        self.__delate_files()
+        self.__delete_files()
 
     @pytest.fixture
     def create_wrong_files(self):
         self.__create_test_dir()
 
-        js = open("tests/test_files/code/project/Main.js", "a")
+        js = open(f"{self.PATH}/code/project/Main.js", "a")
         js.close()
 
-        with open("tests/test_files/input_test.csv", "a") as input:
+        with open(f"{self.PATH}/input_test.csv", "a") as input:
             input.write("file,type,junit\n")
-            input.write("tests/test_files/code/project")
+            input.write(f"{self.PATH}/code/project")
 
         yield
 
-        self.__delate_files()
+        self.__delete_files()
 
     def test_get_file_name(self):
         name = get_file_name("file/myfile.java")
-        assert name == "myfile.java", "file name different than expected, got " + name + " expected myfile.java"
-        
+        assert name == "myfile.java", "file name different than expected, got " + \
+            name + " expected myfile.java"
+
     def test_read_input(self, create_correct_files):
-        files = read_input("tests/test_files/input_test.csv")
+        files = read_input(f"{self.PATH}/input_test.csv")
         assert len(files) == 1, "expected 1, given " + str(len(files))
 
     def test_read_input_fail_1(self):
@@ -75,11 +80,11 @@ class TestUtil:
 
     def test_read_input_fail_2(self, create_empty_csv):
         with pytest.raises(SystemExit):
-            read_input("tests/test_files/input_test.csv")
+            read_input(f"{self.PATH}/input_test.csv")
 
     def test_read_input_fail_3(self, create_wrong_files):
         with pytest.raises(SystemExit):
-            read_input("tests/test_files/input_test.csv")
+            read_input(f"{self.PATH}/input_test.csv")
 
     def test_remove_list_nesting(self):
         """
@@ -95,8 +100,7 @@ class TestUtil:
     def test_get_config_settings_fail_1(self, caplog):
         name = get_config_setting("not existing section", "name")
         assert "not available" in caplog.text, "Not the error message expected"
-    
+
     def test_get_config_settings_fail_2(self, caplog):
         name = get_config_setting("general", "not existing param")
         assert "not available" in caplog.text, "Not the error message expected"
-        
