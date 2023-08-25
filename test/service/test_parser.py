9e5b9f91d6f745e041aba41df9a90e4ff4eb3252
antgioia <antgioia10@gmail.com>
2023-08-25 16:07:26 +0200

    test: delete two test case from parser test delete two test case

--- test/service/test_parser.py
+++ test/service/test_parser.py
@@ -57,30 +57,3 @@ class TestParser:
                 result = parser.parse_file(valid_file_path)
 
         assert result == True
-
-    def test_run_srcml_invalid(self):
-        """
-            ID: TC-SRV-4.1
-        """
-        invalid_file_path = "path/to/invalid/file.java"
-        parser = Parser()
-        result = parser._Parser__run_srcml(invalid_file_path)
-
-        expected_stdout = b''
-        expected_stderr = b'srcml: Unable to open file path/to/invalid/file.java\r\n'
-
-        assert result == (expected_stdout, expected_stderr)
-
-    def test_run_srcml_valid(self, temp_input_file):
-        """
-            ID: TC-SRV-4.2
-        """
-        valid_file_path = str(temp_input_file)
-        parser = Parser()
-        result = parser._Parser__run_srcml(valid_file_path)
-        if isinstance(result, bytes):
-            result_string = result.decode("utf-8")
-        else:
-            result_string = str(result)
-        assert result_string.startswith(
-            '(b\'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>')
