9182cb8408662642a3a674b2d54706d08450a8a3
Antonio Scognamiglio <antonio.scognamiglio12@outlook.it>
2023-09-08 17:01:54 +0200

    test: refactor with autopep8
    refactor test_types_list.py with autopep8

--- test/unit/common/test_types_list.py
+++ test/unit/common/test_types_list.py
@@ -2,52 +2,55 @@ import pytest
 from src.common.types_list import get_primitive_types, get_bool_types, get_numeric_types
 from src.common.enum import LanguageType
 
+
 class TestTypesList:
 
     def test_get_primitive_types_java(self):
         """
             TC-CMM-15.1
         """
-        
+
         # Act
         primitive_types = get_primitive_types(LanguageType.Java)
-        
+
         # Assert
-        assert primitive_types == ['byte', 'short', 'int', 'long', 'float', 'double', 'boolean', 'char']
+        assert primitive_types == ['byte', 'short', 'int',
+                                   'long', 'float', 'double', 'boolean', 'char']
 
     def test_get_primitive_types_csharp(self):
         """
             TC-CMM-15.2
         """
-        
+
         # Act
         primitive_types = get_primitive_types(LanguageType.CSharp)
-        
+
         # Assert
-        assert primitive_types == ['bool', 'byte', 'sbyte', 'char', 'decimal', 'double', 'float', 'int', 'uint', 'long', 'ulong', 'short', 'ushort', 'object', 'string', 'dynamic']
+        assert primitive_types == ['bool', 'byte', 'sbyte', 'char', 'decimal', 'double', 'float',
+                                   'int', 'uint', 'long', 'ulong', 'short', 'ushort', 'object', 'string', 'dynamic']
 
     def test_get_primitive_types_cpp(self):
         """
             TC-CMM-15.3
         """
-        
+
         # Act
         primitive_types = get_primitive_types(LanguageType.CPP)
-        
+
         # Assert
         assert primitive_types == [
-            'int', 
-            'unsigned int', 
-            'signed int', 
-            'short', 
-            'short int', 
-            'unsigned short', 
-            'long', 
-            'long double', 
-            'unsigned long', 
-            'float', 
-            'double', 
-            'char', 
+            'int',
+            'unsigned int',
+            'signed int',
+            'short',
+            'short int',
+            'unsigned short',
+            'long',
+            'long double',
+            'unsigned long',
+            'float',
+            'double',
+            'char',
             'unsigned char',
             'signed char',
             'bool'
@@ -57,43 +60,45 @@ class TestTypesList:
         """
             TC-CMM-15.4
         """
-        
+
         # Act
         primitive_types = get_primitive_types(LanguageType.Unknown)
-        
+
         # Assert
         assert primitive_types == None
-    
+
     def test_get_numeric_types_java(self):
         """
             TC-CMM-16.1
         """
-        
+
         # Act
         numeric_types = get_numeric_types(LanguageType.Java)
-        
+
         # Assert
-        assert numeric_types == ['byte', 'short', 'int', 'long', 'float', 'double', 'Byte', 'Float', 'Integer', 'Long', 'Number', 'Short']
+        assert numeric_types == ['byte', 'short', 'int', 'long', 'float',
+                                 'double', 'Byte', 'Float', 'Integer', 'Long', 'Number', 'Short']
 
     def test_get_numeric_types_csharp(self):
         """
             TC-CMM-16.2
         """
-        
+
         # Act
         numeric_types = get_numeric_types(LanguageType.CSharp)
-        
+
         # Assert
-        assert numeric_types == ['byte', 'sbyte',  'decimal', 'double', 'float', 'int', 'uint', 'long', 'ulong', 'short', 'ushort']
+        assert numeric_types == ['byte', 'sbyte',  'decimal', 'double',
+                                 'float', 'int', 'uint', 'long', 'ulong', 'short', 'ushort']
 
     def test_get_numeric_types_unknown(self):
         """
             TC-CMM-16.3
         """
-        
+
         # Act
         numeric_types = get_numeric_types(LanguageType.Unknown)
-        
+
         # Assert
         assert numeric_types == None
 
@@ -101,10 +106,10 @@ class TestTypesList:
         """
             TC-CMM-17.1
         """
-        
+
         # Act
         bool_types = get_bool_types(LanguageType.Java)
-        
+
         # Assert
         assert bool_types == ['boolean', 'Boolean', 'Predicate']
 
@@ -112,20 +117,20 @@ class TestTypesList:
         """
             TC-CMM-17.2
         """
-        
+
         # Act
         bool_types = get_bool_types(LanguageType.CSharp)
-        
+
         # Assert
         assert bool_types == ['bool', 'Boolean', 'Predicate']
-    
+
     def test_get_bool_types_unknown(self):
         """
             TC-CMM-17.3
         """
-        
+
         # Act
         bool_types = get_bool_types(LanguageType.Unknown)
-        
+
         # Assert
         assert bool_types == None
